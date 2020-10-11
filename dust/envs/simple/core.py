b13b6942c7bf144d5ef53d0e85f7a32acee74bda
Zhehang Ding <zhehangd@gmail.com>
2020-10-11 13:56:52 -0700

    View extraction

--- dust/envs/simple/core.py
+++ dust/envs/simple/core.py
@@ -23,6 +23,49 @@ _REWARD_FOOD = 50
 # The *index* of a position is defined as xi * H + yi, which is used to
 # access the elements of a flattened array.
 
+def _extract_view(src, pos, vr, batched=False):
+    """
+    Args:
+      src N-d source array for view extraction.
+      pos Coordinates of the view center.
+          If batched is False it is an 1-d array of len k, where k <= n.
+          If batched is True it is a 2-d array where the 1st dimension is batch
+      vr int radius of the view
+    """
+    if batched:
+        bsize = len(pos)
+        vsize = len(pos[0])
+    else:
+        bsize = 1
+        vsize = len(pos)
+        pos = (pos,)
+    
+    def make_src_dst_slices(x, w, vr):
+        ss = x - vr
+        se = x + vr + 1 - w
+        ds = de = None
+        if ss < 0: ss, ds = ds, -ss
+        if se > 0: se, de = de, -se
+        if se == 0: se = None
+        return slice(ss, se), slice(ds, de)
+    
+    dst_shape = (bsize,) + (2*vr+1,) * vsize + src.shape[vsize:]
+    dst = np.zeros(dst_shape, src.dtype)
+    for k in range(bsize):
+        src_idxs = []
+        dst_idxs = []
+        for i in range(vsize):
+            p = pos[k][i]
+            assert p >= 0 and p < src.shape[i], '{}, {}, {}'.format(p, i, src.shape)
+            src_idx, dst_idx = make_src_dst_slices(p, src.shape[i], vr)
+            src_idxs.append(src_idx)
+            dst_idxs.append(dst_idx)
+        src_idxs = tuple(src_idxs)
+        dst_idxs = tuple(dst_idxs)
+        dst[k][dst_idxs] = src[src_idxs]
+    if not batched:
+        dst = dst[0]
+    return dst
 
 
 class Env(object):
@@ -47,23 +90,31 @@ class Env(object):
         # Generate random coords in the plane except for the edges
         w = _MAP_WIDTH
         h = _MAP_HEIGHT
-        rand_coords = np.random.permutation((w-2)*(h-2))[:num_objs_cumsum[-1]]
-        rand_coords = rand_coords + 1 + rand_coords // (h-2) * 2 + h 
-        wall_coords, food_coords, player_coords = np.split(
-            rand_coords, num_objs_cumsum[:-1])
+        
+        map_descr = '111111111100000001101101101100000101'\
+                    '111010001100011011100010001101010001'\
+                    '101000111101010101100000001100111011'\
+                    '101100001101001101100000001111111111'
+        map_descr = np.array(list(map_descr), dtype=np.int)
+        wall_coords = np.where(map_descr == 1)[0]
+        empty_coords = np.where(map_descr == 0)[0]
+        np.random.shuffle(empty_coords)
+        
+        food_coords = empty_coords[:_NUM_FOODS]
+        player_coords = empty_coords[_NUM_FOODS:_NUM_FOODS+_NUM_PLAYERS]
         
         # ground:0, wall:1
         map_dtype = [('type', 'i1')]
         map_data = np.zeros(_MAP_SIZE, dtype=map_dtype)
-        map_data[0] = map_data[-1] = map_data[:,0] = map_data[:,-1] = _TYPE_WALL
         map_data_flat = map_data.reshape(-1)
         map_data_flat[wall_coords] = _TYPE_WALL
         self.map_data = map_data
         self.map_data_flat = map_data_flat
         
-        #self.wall_coords = wall_coords
+        self.wall_coords = wall_coords
         self.player_coords = player_coords
         self.food_coords = food_coords
+        
         self.step_reward = 0
         self.total_reward = 0
         self.curr_time = 0
@@ -95,7 +146,15 @@ class Env(object):
         self.curr_time += 1 
         self._reset_action()
         return self.total_reward
-        
+    
+    def index_to_coords(self, idxs):
+        h = _MAP_HEIGHT
+        return np.stack((idxs // h, idxs % h), -1)
+    
+    def extract_obs(self, pos, vr, batched):
+        assert pos.ndim == 2
+        return _extract_view(self.map_data, pos, vr, batched)
+    
     def set_action(self, action):
         self.next_action[:] = action
     
--- dust/simulation.py
+++ dust/simulation.py
@@ -10,7 +10,15 @@ class Agent(object):
         self.num_players = 1
     
     def step(self):
+        
+        # Check any event and calculate the reward from the last stepb 
+        
         # observe
+        env = self.env
+        coords = env.index_to_coords(env.player_coords)
+        obs = env.extract_obs(coords, 2, True)
+        print(obs)
+        
         self.env.set_action(np.random.randint(0, 4, self.num_players))
 
 class SimulationDemo(object):
