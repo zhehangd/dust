63e63e24e6cb0f27cfc679dcd9b1d0cfe315d073
Zhehang Ding <zhehangd@gmail.com>
2020-10-14 18:45:44 -0700

    Clean code

--- dust/dev/agent.py
+++ dust/dev/agent.py
@@ -1,11 +1,13 @@
 import logging
 import os
 
-import numpy as np
-import scipy.signal
-from gym.spaces import Box, Discrete
 import torch
 import torch.nn as nn
+
+import numpy as np
+import scipy.signal
+
+from torch.optim import Adam
 from torch.distributions.normal import Normal
 from torch.distributions.categorical import Categorical
 
@@ -17,32 +19,6 @@ _argparser = _dust.argparser()
 
 _argparser.add_argument('--cuda', action='store_true',
     help='Use CUDA')
-
-def combined_shape(length, shape=None):
-    if shape is None:
-        return (length,)
-    return (length, shape) if np.isscalar(shape) else (length, *shape)
-
-def count_vars(module):
-    return sum([np.prod(p.shape) for p in module.parameters()])
-
-def discount_cumsum(x, discount):
-    """
-    magic from rllab for computing discounted cumulative sums of vectors.
-
-    input: 
-        vector x, 
-        [x0, 
-         x1, 
-         x2]
-
-    output:
-        [x0 + discount * x1 + discount^2 * x2,  
-         x1 + discount * x2,
-         x2]
-    """
-    return scipy.signal.lfilter([1], [1, float(-discount)], x[::-1], axis=0)[::-1]
-
 
 def mlp(sizes, activation=nn.Tanh, output_activation=nn.Identity):
     layers = []
@@ -117,12 +93,6 @@ class MLPActorCritic(nn.Module):
     def act(self, obs):
         return self.step(obs)[0]
 
-import numpy as np
-import torch
-from torch.optim import Adam
-import gym
-import time
-
 class PPOBuffer:
     """
     A buffer for storing trajectories experienced by a PPO agent interacting
@@ -131,6 +101,10 @@ class PPOBuffer:
     """
 
     def __init__(self, obs_dim, act_dim, size, gamma=0.99, lam=0.95):
+        def combined_shape(length, shape=None):
+            if shape is None:
+                return (length,)
+            return (length, shape) if np.isscalar(shape) else (length, *shape)
         self.obs_buf = np.zeros(combined_shape(size, obs_dim), dtype=np.float32)
         self.act_buf = np.zeros(combined_shape(size, act_dim), dtype=np.float32)
         self.adv_buf = np.zeros(size, dtype=np.float32)
@@ -173,6 +147,23 @@ class PPOBuffer:
         rews = np.append(self.rew_buf[path_slice], last_val)
         vals = np.append(self.val_buf[path_slice], last_val)
         
+        def discount_cumsum(x, discount):
+            """
+            magic from rllab for computing discounted cumulative sums of vectors.
+
+            input: 
+                vector x, 
+                [x0, 
+                x1, 
+                x2]
+
+            output:
+                [x0 + discount * x1 + discount^2 * x2,  
+                x1 + discount * x2,
+                x2]
+            """
+            return scipy.signal.lfilter([1], [1, float(-discount)], x[::-1], axis=0)[::-1]
+        
         # the next two lines implement GAE-Lambda advantage calculation
         deltas = rews[:-1] + self.gamma * vals[1:] - vals[:-1]
         self.adv_buf[path_slice] = discount_cumsum(deltas, self.gamma * self.lam)
@@ -201,207 +192,6 @@ class PPOBuffer:
         else:
             return {k: torch.as_tensor(v, dtype=torch.float32) for k,v in data.items()}
 
-"""
-def ppo(env_fn, actor_critic=MLPActorCritic, ac_kwargs=dict(), seed=0, 
-        steps_per_epoch=4000, epochs=50, gamma=0.99, clip_ratio=0.2, pi_lr=3e-4,
-        vf_lr=1e-3, train_pi_iters=80, train_v_iters=80, lam=0.97, max_ep_len=1000,
-        target_kl=0.01, logger_kwargs=dict(), save_freq=10):
-
-    # Special function to avoid certain slowdowns from PyTorch + MPI combo.
-    setup_pytorch_for_mpi()
-
-    # Set up logger and save configuration
-    logger = EpochLogger(**logger_kwargs)
-    logger.save_config(locals())
-
-    # Random seed
-    seed += 10000 * proc_id()
-    torch.manual_seed(seed)
-    np.random.seed(seed)
-
-    # Instantiate environment
-    env = env_fn()
-    obs_dim = env.observation_space.shape
-    act_dim = env.action_space.shape
-
-    # Create actor-critic module
-    ac = actor_critic(env.observation_space, env.action_space, **ac_kwargs)
-
-    # Sync params across processes
-    sync_params(ac)
-
-    # Count variables
-    var_counts = tuple(count_vars(module) for module in [ac.pi, ac.v])
-    logger.log('\nNumber of parameters: \t pi: %d, \t v: %d\n'%var_counts)
-
-    # Set up experience buffer
-    local_steps_per_epoch = int(steps_per_epoch / num_procs())
-    buf = PPOBuffer(obs_dim, act_dim, local_steps_per_epoch, gamma, lam)
-
-    # Set up function for computing PPO policy loss
-    def compute_loss_pi(data):
-        obs, act, adv, logp_old = data['obs'], data['act'], data['adv'], data['logp']
-
-        # Policy loss
-        pi, logp = ac.pi(obs, act)
-        ratio = torch.exp(logp - logp_old)
-        clip_adv = torch.clamp(ratio, 1-clip_ratio, 1+clip_ratio) * adv
-        loss_pi = -(torch.min(ratio * adv, clip_adv)).mean()
-
-        # Useful extra info
-        approx_kl = (logp_old - logp).mean().item()
-        ent = pi.entropy().mean().item()
-        clipped = ratio.gt(1+clip_ratio) | ratio.lt(1-clip_ratio)
-        clipfrac = torch.as_tensor(clipped, dtype=torch.float32).mean().item()
-        pi_info = dict(kl=approx_kl, ent=ent, cf=clipfrac)
-
-        return loss_pi, pi_info
-
-    # Set up function for computing value loss
-    def compute_loss_v(data):
-        obs, ret = data['obs'], data['ret']
-        return ((ac.v(obs) - ret)**2).mean()
-
-    # Set up optims for policy and value function
-    pi_optim = Adam(ac.pi.parameters(), lr=pi_lr)
-    vf_optim = Adam(ac.v.parameters(), lr=vf_lr)
-
-    # Set up model saving
-    logger.setup_pytorch_saver(ac)
-
-    def update():
-        data = buf.get()
-
-        pi_l_old, pi_info_old = compute_loss_pi(data)
-        pi_l_old = pi_l_old.item()
-        v_l_old = compute_loss_v(data).item()
-
-        # Train policy with multiple steps of gradient descent
-        for i in range(train_pi_iters):
-            pi_optim.zero_grad()
-            loss_pi, pi_info = compute_loss_pi(data)
-            kl = mpi_avg(pi_info['kl'])
-            if kl > 1.5 * target_kl:
-                logger.log('Early stopping at step %d due to reaching max kl.'%i)
-                break
-            loss_pi.backward()
-            mpi_avg_grads(ac.pi)    # average grads across MPI processes
-            pi_optim.step()
-
-        logger.store(StopIter=i)
-
-        # Value function learning
-        for i in range(train_v_iters):
-            vf_optim.zero_grad()
-            loss_v = compute_loss_v(data)
-            loss_v.backward()
-            mpi_avg_grads(ac.v)    # average grads across MPI processes
-            vf_optim.step()
-
-        # Log changes from update
-        kl, ent, cf = pi_info['kl'], pi_info_old['ent'], pi_info['cf']
-        logger.store(LossPi=pi_l_old, LossV=v_l_old,
-                     KL=kl, Entropy=ent, ClipFrac=cf,
-                     DeltaLossPi=(loss_pi.item() - pi_l_old),
-                     DeltaLossV=(loss_v.item() - v_l_old))
-
-    # Prepare for interaction with environment
-    start_time = time.time()
-    o, ep_ret, ep_len = env.reset(), 0, 0
-
-    # Main loop: collect experience in env and update/log each epoch
-    for epoch in range(epochs):
-        for t in range(local_steps_per_epoch):
-            a, v, logp = ac.step(torch.as_tensor(o, dtype=torch.float32))
-
-            next_o, r, d, _ = env.step(a)
-            ep_ret += r
-            ep_len += 1
-
-            # save and log
-            buf.store(o, a, r, v, logp)
-            logger.store(VVals=v)
-            
-            # Update obs (critical!)
-            o = next_o
-
-            timeout = ep_len == max_ep_len
-            terminal = d or timeout
-            epoch_ended = t==local_steps_per_epoch-1
-
-            if terminal or epoch_ended:
-                if epoch_ended and not(terminal):
-                    print('Warning: trajectory cut off by epoch at %d steps.'%ep_len, flush=True)
-                # if trajectory didn't reach terminal state, bootstrap value target
-                if timeout or epoch_ended:
-                    _, v, _ = ac.step(torch.as_tensor(o, dtype=torch.float32))
-                else:
-                    v = 0
-                buf.finish_path(v)
-                if terminal:
-                    # only save EpRet / EpLen if trajectory finished
-                    logger.store(EpRet=ep_ret, EpLen=ep_len)
-                o, ep_ret, ep_len = env.reset(), 0, 0
-
-
-        # Save model
-        if (epoch % save_freq == 0) or (epoch == epochs-1):
-            logger.save_state({'env': env}, None)
-
-        # Perform PPO update!
-        update()
-
-        # Log info about epoch
-        logger.log_tabular('Epoch', epoch)
-        logger.log_tabular('EpRet', with_min_and_max=True)
-        logger.log_tabular('EpLen', average_only=True)
-        logger.log_tabular('VVals', with_min_and_max=True)
-        logger.log_tabular('TotalEnvInteracts', (epoch+1)*steps_per_epoch)
-        logger.log_tabular('LossPi', average_only=True)
-        logger.log_tabular('LossV', average_only=True)
-        logger.log_tabular('DeltaLossPi', average_only=True)
-        logger.log_tabular('DeltaLossV', average_only=True)
-        logger.log_tabular('Entropy', average_only=True)
-        logger.log_tabular('KL', average_only=True)
-        logger.log_tabular('ClipFrac', average_only=True)
-        logger.log_tabular('StopIter', average_only=True)
-        logger.log_tabular('Time', time.time()-start_time)
-        logger.dump_tabular()
-
-if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--env', type=str, default='HalfCheetah-v2')
-    parser.add_argument('--hid', type=int, default=64)
-    parser.add_argument('--l', type=int, default=2)
-    parser.add_argument('--gamma', type=float, default=0.99)
-    parser.add_argument('--seed', '-s', type=int, default=0)
-    parser.add_argument('--cpu', type=int, default=4)
-    parser.add_argument('--steps', type=int, default=4000)
-    parser.add_argument('--epochs', type=int, default=50)
-    parser.add_argument('--exp_name', type=str, default='ppo')
-    args = parser.parse_args()
-
-    mpi_fork(args.cpu)  # run parallel code with mpi
-
-    from spinup.utils.run_utils import setup_logger_kwargs
-    logger_kwargs = setup_logger_kwargs(args.exp_name, args.seed)
-
-    ppo(lambda : gym.make(args.env), actor_critic=MLPActorCritic,
-        ac_kwargs=dict(hidden_sizes=[args.hid]*args.l), gamma=args.gamma, 
-        seed=args.seed, steps_per_epoch=args.steps, epochs=args.epochs,
-        logger_kwargs=logger_kwargs)
-"""
-
-import dust
-
-import numpy as np
-
-import time
-
-
-def index_to_coords(idxs, h):
-    return np.stack((idxs // h, idxs % h), -1)
 
 # Set up function for computing PPO policy loss
 def compute_loss_pi(ac, data):
@@ -477,6 +267,8 @@ class Agent(object):
         ac = MLPActorCritic(25, 4)
         self.ac = ac
         
+        def count_vars(module):
+            return sum([np.prod(p.shape) for p in module.parameters()])
         var_counts = tuple(count_vars(module) for module in [ac.pi, ac.v])
         logging.info('\nNumber of parameters: \t pi: %d, \t v: %d\n'%var_counts)
         
@@ -501,6 +293,8 @@ class Agent(object):
     def _get_observation(self):
         """ Extract observation from the current env
         """
+        def index_to_coords(idxs, h):
+            return np.stack((idxs // h, idxs % h), -1)
         env = self.env
         # Generate vision at each player position
         coords = index_to_coords(env.player_coords, env.map_shape[1])
