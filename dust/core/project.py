dbd5907aaa74e5daa83c4e9dd32f1271f094f29d
Zhehang Ding <zhehangd@gmail.com>
2020-12-15 02:12:59 -0800

    Improve project behavior

--- dust/core/project.py
+++ dust/core/project.py
@@ -3,6 +3,8 @@ import datetime
 import logging
 import os
 import sys
+import tempfile
+
 import toml
 
 import dust.core.init
@@ -19,16 +21,21 @@ class Project(object):
     Projects are configured by args.
     """
     
-    def __init__(self, sess_name, proj_dir, timestamp, init):
-        self.timestamp = datetime.datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
-        self.proj_dir = os.getcwd()
-        self.sess_name = sess_name
+    def __init__(self, load_proj: bool, **kwargs):
+        global _PROJECT
+        assert _PROJECT is None, 'Project has been created'
+        _PROJECT = self
         
+        self._timestamp = kwargs.get('timestamp',
+            datetime.datetime.today().strftime('%Y-%m-%d-%H-%M-%S'))
+        self._proj_dir = kwargs.get('proj_dir', os.getcwd())
+        self._sess_name = kwargs.get('sess_name', 'default')
+        self._init_logger()
         
         proj_file = os.path.join(self.proj_dir, _PROJECT_FILE)
         cfg = arg_and_cfg_parser.Namespace()
         args = arg_and_cfg_parser.Namespace()
-        if not init:
+        if load_proj:
             if not os.path.isfile(proj_file):
                 raise RuntimeError('{} is not found in "{}"'\
                     .format(_PROJECT_FILE, self.proj_dir))
@@ -36,7 +43,44 @@ class Project(object):
             cfg.__dict__.update(proj_dict['cfg'])
         self.cfg = cfg
         self.args = args
+        self._global = True
+    
+    def release(self):
+        """ Detaches the project from the global project position
         
+        This removes the project from the global project variable,
+        and reset the logger it maintains, making the project a
+        normal object without global effect.
+        
+        The only scenario this method should be called is when you
+        want to switch projects for some special reasons (like testing).
+        
+        You may call this method only when this project is the global one.
+        
+        """
+        assert self._global
+        global _PROJECT
+        assert _PROJECT == self
+        _PROJECT = None
+        self._reset_logger()
+        self._global = False
+    
+    @property
+    def log_filename(self):
+        return self._log_filename
+    
+    @property
+    def proj_dir(self):
+        return self._proj_dir
+    
+    @property
+    def sess_name(self):
+        return self._sess_name
+    
+    @property
+    def timestamp(self):
+        return self._timestamp
+    
     def parse_args(self, args=None, allow_unknown=False):
         parser = dust.core.init.argparser()
         args, cfg = parser.parse_args(args, self.cfg, allow_unknown=allow_unknown)
@@ -55,6 +99,37 @@ class Project(object):
             proj_dict = {}
             proj_dict['cfg'] = self.cfg.__dict__
             toml.dump(proj_dict, f)
+    
+    def _init_logger(self):
+        self._reset_logger()
+        logger = logging.getLogger(None)
+        logger.setLevel(logging.INFO)
+        
+        # e.g. 2020-10-08 22:26:03,509 INFO
+        formatter = logging.Formatter('%(asctime)s %(levelname)-7s %(message)s')
+        
+        # Log to STDERR
+        handler = logging.StreamHandler(sys.stderr)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+        # Log to file
+        log_basename = 'log.{}.{}.log'.format(self.sess_name, self.timestamp)
+        log_filename = os.path.join(self.proj_dir, 'logs', log_basename)
+        self._log_filename = log_filename
+        
+        os.makedirs(os.path.dirname(os.path.abspath(log_filename)), exist_ok=True)
+        handler = logging.FileHandler(log_filename)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    def _reset_logger(self):
+        assert logging
+        logger = logging.getLogger(None)
+        for handler in logger.handlers[:]:
+            logger.removeHandler(handler)
+        for filter in logger.filters[:]:
+            logger.removeFilter(filter)
 
 def inside_project():
     return isinstance(_PROJECT, Project)
@@ -66,41 +141,37 @@ def project():
     assert isinstance(_PROJECT, Project), 'You haven\'t loaded a project.'
     return _PROJECT
 
-def _create_project(sess_name, init):
+def _create_project(sess_name, proj_dir, init) -> Project:
     """ Inits and enters a project
     """
-    global _PROJECT
-    assert _PROJECT is None, 'Project has been created'
     
-    timestamp = datetime.datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
-    proj_dir = os.getcwd()
-    
-    log_file = os.path.join(proj_dir, 'logs', 'log.{}.{}.log'.format(sess_name, timestamp))
-    _setup_project_logger(log_file) # One may use logging after this line
     _PROJECT = Project(sess_name=sess_name, proj_dir=proj_dir,
-                   timestamp=timestamp, init=init)
+                       timestamp=timestamp, init=init)
     return _PROJECT
 
-def create_project(sess_name='default'):
-    return _create_project(sess_name, True)
+def create_project(**kwargs) -> Project:
+    return Project(False, **kwargs)
 
-def load_project(sess_name='default'):
-    return _create_project(sess_name, False)
+def load_project(**kwargs) -> Project:
+    return Project(True, **kwargs)
 
-def _setup_project_logger(log_file=None):
-    logger = logging.getLogger(None)
-    logger.setLevel(logging.INFO)
-    # e.g. 2020-10-08 22:26:03,509 INFO
-    formatter = logging.Formatter('%(asctime)s %(levelname)-7s %(message)s')
-    
-    # Log to STDERR
-    handler = logging.StreamHandler(sys.stderr)
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+def create_temporary_project(**kwargs) -> Project:
+    temp_dir_obj = tempfile.TemporaryDirectory()
+    proj_dir = temp_dir_obj.name
+    kwargs['proj_dir'] = proj_dir
+    proj = Project(False, **kwargs)
+    assert proj.proj_dir == proj_dir, '{} vs. {}'.format(proj.proj_dir, proj_dir)
+    # Upon the destruction of the project, this object is also destructed
+    # as well as the temporary directory.
+    proj._temp_dir_obj = temp_dir_obj 
+    return proj
 
-    # Log to file
-    if log_file:
-        os.makedirs(os.path.dirname(os.path.abspath(log_file)), exist_ok=True)
-        handler = logging.FileHandler(log_file)
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
+def close_project():
+    global _PROJECT
+    assert isinstance(_PROJECT, Project)
+    proj, _PROJECT = _PROJECT, None
+    del proj
+
+def detach_global_project():
+    if _PROJECT:
+        _PROJECT.release()
---
+++ dust/core/test_project.py
@@ -0,0 +1,27 @@
+import os
+
+from dust.core import project
+
+def check_project(proj):
+    assert proj.proj_dir
+    assert os.path.isdir(proj.proj_dir)
+    assert proj.log_filename
+    assert os.path.isfile(proj.log_filename)
+    assert proj.log_filename.startswith(proj.proj_dir)
+
+def test_create_temporary_project():
+    proj = project.create_temporary_project()
+    check_project(proj)
+    proj_dir = proj.proj_dir
+    proj.release()
+    del proj
+    assert not os.path.isdir(proj_dir), proj_dir
+
+def test_detach_global_project():
+    project.detach_global_project()
+    proj = project.create_temporary_project()
+    project.detach_global_project()
+    proj = project.create_temporary_project()
+    project.detach_global_project()
+
+
